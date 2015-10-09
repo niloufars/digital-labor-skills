40495775de13b901c7023715e9cdaf6cd722759d
niloufars <niloufar.salehi@gmail.com>
2015-10-09 13:25:15 -0700

    x-frame

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,7 @@ module DigitalLaborSkills
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     
-    config.action_dispatch.default_headers = {
-    'X-Frame-Options' => 'ALLOWALL'
-    }
+    config.action_dispatch.default_headers.clear
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
